c710caeca305e7e8bc9ea906190691e8a6b151c3
NFS2245 <e41211246@student.polije.ac.id>
2022-10-23 19:07:38 +0700

    First Commit

---
+++ lib/splash.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Image.asset(
+            'assets/logo.png',
+            scale: 1.5,
+          ),
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Text('')
+      ],
+    ));
+  }
+}
